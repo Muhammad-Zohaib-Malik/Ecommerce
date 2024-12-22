e06bc57541d72f15c8f75ce207af841964c6621e
zohaib <muhammadzohaibmalik10@gmail.com>
2024-12-22 17:48:47 +0500

    Add mongoose  and implement database connection in server setup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 
 dotenv.config();
 
@@ -9,6 +10,9 @@ const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
+
+// connection to db
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
