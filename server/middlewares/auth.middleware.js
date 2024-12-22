7ffa457373d94b3a06720bb5900e9e3e4c9808d4
zohaib <muhammadzohaibmalik10@gmail.com>
2024-12-22 22:22:01 +0500

    fix bugs add next paramter

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -2,7 +2,7 @@ import { User } from '../models/user.model.js';
 import { ApiError } from '../utils/ApiError.js';
 import jwt from 'jsonwebtoken';
 
-export const verify = async (req, _) => {
+export const verify = async (req, _, next) => {
   try {
     const token = req.cookies?.accessToken;
     if (!token) {
