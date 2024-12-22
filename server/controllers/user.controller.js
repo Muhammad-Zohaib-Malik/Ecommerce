0ccfc2b5b807c981ef395b96b878ab154e484368
zohaib <muhammadzohaibmalik10@gmail.com>
2024-12-22 21:10:19 +0500

    added login Controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -31,3 +31,41 @@ export const registerController = asyncHandler(async (req, res) => {
     .status(201)
     .json(new ApiResponse(200, createdUser, 'User registered Successfully'));
 });
+
+export const loginController = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new ApiError(400, 'email and password is required');
+  }
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new ApiError(404, 'user does not exists');
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, 'Invalid user credentials');
+  }
+
+  const accessToken = user.generateAccessToken();
+
+  const loggedInUser = await User.findById(user._id).select('-password');
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie('accessToken', accessToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        { user: loggedInUser, accessToken },
+        'User loggedIn successfully'
+      )
+    );
+});
