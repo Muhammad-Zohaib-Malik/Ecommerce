0ab8e1c720afca1543a53438a56ca5474aac4a74
zohaib <muhammadzohaibmalik10@gmail.com>
2024-12-28 12:01:04 +0500

    added middleware in route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import {
+  getUserProfileController,
   loginController,
   registerController,
 } from '../controllers/user.controller.js';
-
+import { verify } from '../middlewares/auth.middleware.js';
 const userRouter = express.Router();
 
 userRouter.post('/register', registerController);
 userRouter.post('/login', loginController);
+userRouter.get('/profile', verify, getUserProfileController);
 
 export default userRouter;
