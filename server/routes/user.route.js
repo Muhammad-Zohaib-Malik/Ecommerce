8d02c7084a917c3d92589b92f5d0a73d33906fe1
zohaib <muhammadzohaibmalik10@gmail.com>
2025-01-01 18:44:00 +0500

    Implement password update route in user router

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -4,6 +4,7 @@ import {
   loginController,
   logoutController,
   registerController,
+  updatePasswordController,
   updateProfileController,
 } from '../controllers/user.controller.js';
 import { verify } from '../middlewares/auth.middleware.js';
@@ -13,8 +14,7 @@ userRouter.post('/register', registerController);
 userRouter.post('/login', loginController);
 userRouter.get('/profile', verify, getUserProfileController);
 userRouter.get('/logout', logoutController);
-userRouter.put('/profile-update',verify, updateProfileController);
-
-
+userRouter.put('/profile-update', verify, updateProfileController);
+userRouter.put('/update-password', verify, updatePasswordController);
 
 export default userRouter;
