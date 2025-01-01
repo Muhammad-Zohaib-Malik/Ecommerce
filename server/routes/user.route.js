859f82f50febd8ec5bb2ac6baef859a489e4b751
zohaib <muhammadzohaibmalik10@gmail.com>
2025-01-01 21:03:30 +0500

    Refactor user routes: Rename profile update endpoint and add profile picture update route with multer integration

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -6,15 +6,23 @@ import {
   registerController,
   updatePasswordController,
   updateProfileController,
+  updateProfilePicController,
 } from '../controllers/user.controller.js';
 import { verify } from '../middlewares/auth.middleware.js';
+import { upload } from '../middlewares/multer.js';
 const userRouter = express.Router();
 
 userRouter.post('/register', registerController);
 userRouter.post('/login', loginController);
 userRouter.get('/profile', verify, getUserProfileController);
 userRouter.get('/logout', logoutController);
-userRouter.put('/profile-update', verify, updateProfileController);
+userRouter.put('/update-profile', verify, updateProfileController);
 userRouter.put('/update-password', verify, updatePasswordController);
+userRouter.put(
+  '/update-profile-pic',
+  verify,
+  upload.single('profilePic'),
+  updateProfilePicController
+);
 
 export default userRouter;
