5cdb64b4be4222e8db17cb1ff7c3cb38706f9b01
zohaib <muhammadzohaibmalik10@gmail.com>
2025-01-02 16:47:59 +0500

    Add timeStamps

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -1,44 +1,47 @@
 import mongoose from 'mongoose';
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Product name is required'],
-    trim: true,
-  },
-  description: {
-    type: String,
-    required: [true, 'Product description is required'],
-    trim: true,
-  },
-  price: {
-    type: Number,
-    required: [true, 'Product price is required'],
-    min: [0, 'Product price must be a positive number'],
-    default: 0,
-  },
-  stock: {
-    type: Number,
-    required: [true, 'Product stock is required'],
-    min: [0, 'Product stock must be a positive number'],
-    default: 0,
-  },
-  category: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'category',
-  },
-  images: [
-    {
+const productSchema = new mongoose.Schema(
+  {
+    name: {
       type: String,
-      required: [true, 'Product image URL is required'],
+      required: [true, 'Product name is required'],
+      trim: true,
     },
-  ],
-  imageId: [
-    {
+    description: {
       type: String,
-      required: [true, 'Product image ID is required'],
+      required: [true, 'Product description is required'],
+      trim: true,
     },
-  ],
-});
+    price: {
+      type: Number,
+      required: [true, 'Product price is required'],
+      min: [0, 'Product price must be a positive number'],
+      default: 0,
+    },
+    stock: {
+      type: Number,
+      required: [true, 'Product stock is required'],
+      min: [0, 'Product stock must be a positive number'],
+      default: 0,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'category',
+    },
+    images: [
+      {
+        type: String,
+        required: [true, 'Product image URL is required'],
+      },
+    ],
+    imageId: [
+      {
+        type: String,
+        required: [true, 'Product image ID is required'],
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 export const Product = mongoose.model('Product', productSchema);
