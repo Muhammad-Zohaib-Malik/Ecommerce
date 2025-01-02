47cbea95cdb41518c6e1b5c4e3a41c8458f2c82a
zohaib <muhammadzohaibmalik10@gmail.com>
2025-01-02 16:44:29 +0500

    Add product model

---
+++ server/models/product.model.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Product name is required'],
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, 'Product description is required'],
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Product price is required'],
+    min: [0, 'Product price must be a positive number'],
+    default: 0,
+  },
+  stock: {
+    type: Number,
+    required: [true, 'Product stock is required'],
+    min: [0, 'Product stock must be a positive number'],
+    default: 0,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'category',
+  },
+  images: [
+    {
+      type: String,
+      required: [true, 'Product image URL is required'],
+    },
+  ],
+  imageId: [
+    {
+      type: String,
+      required: [true, 'Product image ID is required'],
+    },
+  ],
+});
+
+export const Product = mongoose.model('Product', productSchema);
