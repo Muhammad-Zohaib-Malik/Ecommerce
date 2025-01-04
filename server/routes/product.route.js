22fa9891bf3b8cebc76ec49cba1e91b6b0fa080d
zohaib <muhammadzohaibmalik10@gmail.com>
2025-01-04 14:24:29 +0500

    Add create product route

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import {
+  createProductController,
   getAllProductsController,
   getSingleProductsController,
 } from '../controllers/product.controller.js';
+import { verify } from '../middlewares/auth.middleware.js';
+import { upload } from '../middlewares/multer.js';
 
 const productRouter = express.Router();
 
 productRouter.get('/get-all', getAllProductsController);
 productRouter.get('/:id', getSingleProductsController);
+productRouter.post(
+  '/create',
+  verify,
+  upload.array('images', 4),
+  createProductController
+);
 
 export default productRouter;
