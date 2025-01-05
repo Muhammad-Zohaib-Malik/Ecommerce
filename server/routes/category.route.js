e128e95139c96700a7f9c89e9b41a65f177f50d2
zohaib <muhammadzohaibmalik10@gmail.com>
2025-01-05 20:46:46 +0500

    added updated route

--- server/routes/category.route.js
+++ server/routes/category.route.js
@@ -5,11 +5,13 @@ import {
   createCategory,
   deleteCategoryController,
   getCategoryController,
+  updateCategoryController,
 } from '../controllers/category.controller.js';
 const categoryRouter = express.Router();
 
 categoryRouter.post('/create', verify, createCategory);
 categoryRouter.get('/get-all', getCategoryController);
 categoryRouter.delete('/delete/:id', verify, deleteCategoryController);
+categoryRouter.put('/update/:id', verify, updateCategoryController); // product Id
 
 export default categoryRouter;
