23b8ee1c96ecfec72d9b08b637eb811a5a2b631d
zohaib <muhammadzohaibmalik10@gmail.com>
2025-01-05 19:58:18 +0500

    Add deleteCategoryController

--- server/controllers/category.controller.js
+++ server/controllers/category.controller.js
@@ -1,6 +1,8 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
 import { ApiError } from '../utils/ApiError.js';
 import { Category } from '../models/category.model.js';
+import { Product } from '../models/product.model.js';
+
 import { ApiResponse } from '../utils/ApiResponse.js';
 
 export const createCategory = asyncHandler(async (req, res) => {
@@ -24,4 +26,27 @@ export const getCategoryController = asyncHandler(async (req, res) => {
   return res.json(
     new ApiResponse(200, category, 'Category fetched Successfully')
   );
+});
+
+export const deleteCategoryController = asyncHandler(async (req, res) => {
+  const categoryId = req.params.id;
+
+  if (!categoryId) {
+    throw new ApiError(400, 'Category ID is required');
+  }
+
+  const category = await Category.findById(categoryId);
+
+  if (!category) {
+    throw new ApiError(404, 'Category not found');
+  }
+
+  await Product.updateMany(
+    { category: categoryId },
+    { $unset: { category: '' } }
+  );
+
+  await Category.findByIdAndDelete(category);
+
+  return res.json(new ApiResponse(200, null, 'Category deleted successfully'));
 });
