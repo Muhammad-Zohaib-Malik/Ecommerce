5c9c790e2bc3e4aadb985a1e59ddb10e81c73032
zohaib <muhammadzohaibmalik10@gmail.com>
2025-01-05 19:58:42 +0500

    added delete route

--- server/routes/category.route.js
+++ server/routes/category.route.js
@@ -3,11 +3,13 @@ import express from 'express';
 import { verify } from '../middlewares/auth.middleware.js';
 import {
   createCategory,
+  deleteCategoryController,
   getCategoryController,
 } from '../controllers/category.controller.js';
 const categoryRouter = express.Router();
 
 categoryRouter.post('/create', verify, createCategory);
 categoryRouter.get('/get-all', getCategoryController);
+categoryRouter.delete('/delete/:id', verify, deleteCategoryController);
 
 export default categoryRouter;
