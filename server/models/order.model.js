d0a0f5eb038d70141447facd4b9e408228a33dae
zohaib <muhammadzohaibmalik10@gmail.com>
2025-01-15 20:01:58 +0500

    Order Model Added

---
+++ server/models/order.model.js
@@ -0,0 +1,95 @@
+import mongoose from 'mongoose';
+
+const orderSchmea = new mongoose.Schema({
+  shippingInfo: {
+    address: {
+      type: String,
+      required: [true, 'address is required'],
+    },
+    city: {
+      type: String,
+      required: [true, 'city is required'],
+    },
+    country: {
+      type: String,
+      required: [true, 'country is required'],
+    },
+  },
+
+  orderItems: [
+    {
+      name: {
+        type: String,
+        required: [true, 'Product is required'],
+      },
+      price: {
+        type: Number,
+        required: [true, 'Product Price is required'],
+      },
+      quantity: {
+        type: Number,
+        required: [true, 'Product Quantity  is required'],
+      },
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: [true, 'Product ID is required'],
+      },
+      images: [
+        {
+          type: String,
+          required: [true, 'Product image URL is required'],
+        },
+      ],
+      imageId: [
+        {
+          type: String,
+          required: [true, 'Product image ID is required'],
+        },
+      ],
+    },
+  ],
+
+  paymentMethod: {
+    type: String,
+    enum: ['COD', 'ONLINE'],
+    default: 'COD',
+  },
+
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'user Id is required'],
+  },
+
+  paidAt: Date,
+  paymentInfo: {
+    id: String,
+    status: String,
+  },
+  itemPrice: {
+    type: Number,
+    required: [true, 'Item Price is required'],
+  },
+  tax: {
+    type: Number,
+    required: [true, 'tax Price is required'],
+  },
+  shippingCharges: {
+    type: Number,
+    required: [true, 'Item Shipping Charges is required'],
+  },
+
+  totalAmount: {
+    type: Number,
+    required: [true, 'Item total Amount  is required'],
+  },
+  orderStatus: {
+    type: String,
+    enum: ['processing', 'shipped', 'deliverd'],
+    default: 'processing',
+  },
+  deliverdAt: Date,
+});
+
+export const Order = mongoose.model('Order', orderSchmea);
